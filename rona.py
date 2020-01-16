630c5f780e021f279e157b436cae43dcbccf85f5
fataelislami <fataelislami97@gmail.com>
2020-01-16 18:16:07 +0700

    Change

--- rona.py
+++ rona.py
@@ -1,18 +1,7 @@
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
-
 app = Flask(__name__)
-
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Start our response
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message("Ahoy! Thanks so much for your message.")
-
-    return str(resp)
-
+@app.route("/")
+def hello():
+    return "Hello worsd!"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
