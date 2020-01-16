4ffadb5e9b4706420a983a7221fb692115b92014
fataelislami <fataelislami97@gmail.com>
2020-01-16 16:58:07 +0700

    Update rona init

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from rona import create_app
+application = create_app()
--- rona.py
+++ rona.py
@@ -2,6 +2,6 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "Hello worsd!"
 if __name__ == "__main__":
     app.run()
